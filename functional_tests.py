1f6da7717c57f51c65acfed18fd61c84fec14204
TranDavid1 <dtrancpp@gmail.com>
2023-03-23 14:35:17 -0700

    create functional_tests.py, create Django project

---
+++ functional_tests.py
@@ -0,0 +1,7 @@
+# start selenium webdriver to create Firefox browser window
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
