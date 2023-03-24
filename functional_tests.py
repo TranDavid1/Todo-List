ab04a0cf8400be1fb86e3822e20f3aa97c93033b
TranDavid1 <dtrancpp@gmail.com>
2023-03-23 23:23:17 -0700

    "first functional test added into some of the TODO comments, now uses unittest"

--- functional_tests.py
+++ functional_tests.py
@@ -1,15 +1,34 @@
 # start selenium webdriver to create Firefox browser window
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# Home page
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# Page title and header mention to-do lists
-assert 'To-Do' in browser.title
+    # start browser before/after each test
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# Invite to enter a to-do item immediately
+    # stop browser before/after each test, tearDown should run even if there's an error
+    # in the test itself
+    def tearDown(self):
+        self.browser.quit()
+
+    # test method, run by test runner
+    def test_can_start_a_list_and_retrieve_it(self):
+        # Home page
+        self.browser.get('http://localhost:8000')
+
+        # Page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish test!')
+
+        # Invite to enter a to-do item immediately
+        # [...rest of comments as before]
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 # Type entry into a text box
 
@@ -24,4 +43,4 @@ assert 'To-Do' in browser.title
 
 # Upon visiting the URL, the to-do list with 2 items should still be there
 
-browser.quit()
+# browser.quit()
