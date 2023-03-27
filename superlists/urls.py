b339c101e85db4a0039bb817c6da78a2284b11c8
TranDavid1 <dtrancpp@gmail.com>
2023-03-27 16:55:17 -0700

    fix 301 redirect in /superlists/urls.py

--- superlists/urls.py
+++ superlists/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/new$', views.new_list, name='new_list'),
-    re_path(r'^lists/(.+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
 ]
