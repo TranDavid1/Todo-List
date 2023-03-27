337b0215bef618eb6f85977cfb52aabd5d4b2a90
TranDavid1 <dtrancpp@gmail.com>
2023-03-27 16:53:53 -0700

    UT fails because of greedy regex in /lists/tests.py, NewItemTest class

--- lists/tests.py
+++ lists/tests.py
@@ -42,22 +42,22 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
 
-class NewListTest(TestCase):
-    def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+# class NewListTest(TestCase):
+#     def test_can_save_a_POST_request(self):
+#         self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
+#         self.assertEqual(Item.objects.count(), 1)
+#         new_item = Item.objects.first()
+#         self.assertEqual(new_item.text, 'A new list item')
 
-    def test_redirects_after_POST(self):
-        response = self.client.post(
-            '/lists/new', data={'item_text': 'A new list item'})
-        new_list = List.objects.first()
-        self.assertRedirects(response, f'/lists/{new_list.id}/')
-        # self.assertEqual(response.status_code, 302)
-        # self.assertEqual(response['location'],
-        #                  '/lists/the-only-list-in-the-world/')
+#     def test_redirects_after_POST(self):
+#         response = self.client.post(
+#             '/lists/new', data={'item_text': 'A new list item'})
+#         new_list = List.objects.first()
+#         self.assertRedirects(response, f'/lists/{new_list.id}/')
+#         # self.assertEqual(response.status_code, 302)
+#         # self.assertEqual(response['location'],
+#         #                  '/lists/the-only-list-in-the-world/')
 
 
 class ListAndItemModelsTest(TestCase):
@@ -87,3 +87,30 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_test': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/ add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
