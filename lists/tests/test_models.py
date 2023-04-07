c168a451efd300041e9780e67190f8a062784544
TranDavid1 <dtrancpp@gmail.com>
2023-04-06 20:34:17 -0700

    create get_absolute_url function, UT pass

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -45,3 +45,7 @@ class ListAndItemModelsTest(TestCase):
             # Django models don't run full validation on save,
             # full_clean() performs that manually
             item.full_clean()
+
+    def test_get_absolute_url(self):
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
