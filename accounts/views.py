99f47a26db3dd3f868af822b7a23d1212b1c6f06
TranDavid1 <dtrancpp@gmail.com>
2023-04-20 03:59:55 -0700

    basic unit test for mock email

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+import uuid
+import sys
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login, logout as auth_logout
+# from accounts.models import Token, ListUser
+from accounts.models import Token
 
 # Create your views here.
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists', 
+        'body text tbc',
+        'noreply@superlists', 
+        [email]
+    )
+    # uid = str(uuid.uuid4())
+    # Token.objects.create(email=email, uid=uid)
+    # # ListUser.objects.create(email=email)
+    # print('saving uid', uid, 'for email', email, file=sys.stderr)
+    # url = request.build_absolute_uri(f'/accounts/login?uid={uid}')
+    # send_mail(
+    #     'Your login link for Superlists',
+    #     f'Use this link to log in:\n\n{url}',
+    #     'noreply@superlists',
+    #     [email],
+    # )
+    # return render(request, 'login_email_sent.html')
+    return redirect('/')
+
+# def login(request):
+#     print('login view', file=sys.stderr)
+#     uid = request.GET.get('uid')
+#     print("uid:",uid)
+#     # print(type(uid))
+#     user = authenticate(uid=uid)
+#     print(authenticate(uid=uid))
+#     if user is not None:
+#         auth_login(request, user)
+#     return redirect('/')
+
+# def logout(request):
+#     auth_logout(request)
+#     return redirect('/')
