3c3b8f7adb808dfcbe121bd7053ad0a8d23c8c0e
TranDavid1 <dtrancpp@gmail.com>
2023-04-20 19:54:04 -0700

    use token in url in message body

--- accounts/views.py
+++ accounts/views.py
@@ -5,14 +5,19 @@ from django.core.mail import send_mail
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.urls import reverse
 # from accounts.models import Token, ListUser
 from accounts.models import Token
 
 # Create your views here.
 def send_login_email(request):
     email = request.POST['email']
-    # print(type(send_mail))
     token = Token.objects.create(email=email)
+    # print(type(send_mail))
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists', 
         'Use this link to log in',
