e75483d8169b83ed6ec8f0b3c7bb3bcf6a5dca25
TranDavid1 <dtrancpp@gmail.com>
2023-04-25 00:53:29 -0700

    fix logout in base.html and accounts.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import re_path
 from accounts import views
-from django.contrib.auth import logout
+# from django.contrib.auth import logout
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     re_path(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     re_path(r'^login$', views.login, name='login'),
-    re_path(r'^logout$', logout, {'next_page': '/'}, name='logout')
+    re_path(
+        r'^logout$', 
+        auth_views.LogoutView.as_view(), 
+        {'next_page': '/'}, 
+        name='logout'
+    )
+    # re_path('^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
